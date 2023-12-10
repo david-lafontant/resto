30fb0d390b9eae849618bcd45bafe303c1816a30
david-lafontant <lafontant.david@gmail.com>
2023-12-10 15:28:09 -0500

    Setup route to create a restaurant

--- app/api/restaurants/route.ts
+++ app/api/restaurants/route.ts
@@ -5,3 +5,12 @@ export async function GET(request: Request) {
   const restaurants = await prisma.restaurant.findMany();
   return NextResponse.json(restaurants);
 }
+
+export async function POST(request: Request) {
+  const json = await request.json();
+  const created = await prisma.restaurant.create({
+    data: json
+  });
+  return new NextResponse(JSON.stringify(created), { status: 201 });
+
+}
