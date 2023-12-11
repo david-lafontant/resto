80b8189ada878a8a2fbf9e47c8889d2c5212ef54
david-lafontant <lafontant.david@gmail.com>
2023-12-11 04:47:45 -0500

    Display menu and menu item in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import { prisma } from '@/lib/prisma';
 
 export default async function Home() {
   const user = await prisma.user.findFirst();
-  const restaurant = await prisma.restaurant.findFirst()
+  const restaurant = await prisma.restaurant.findFirst();
+  const menu = await prisma.menu.findFirst();
+  const menuItem = await prisma.menuItem.findFirst();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello, {user?.name}</h1>
       <p> I am eating at {restaurant?.name}</p>
+      <p>I ordered a {menuItem?.name} from the {menu?.name} menu</p>
     </main>
   );
 }
