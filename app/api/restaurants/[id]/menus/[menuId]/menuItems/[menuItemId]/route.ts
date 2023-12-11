52ed5df2e6201fcacafd0e641e86fca08fcb09a0
david-lafontant <lafontant.david@gmail.com>
2023-12-11 09:12:17 -0500

    Setup route to update (PUT) a menuItem

--- app/api/restaurants/[id]/menus/[menuId]/menuItems/[menuItemId]/route.ts
+++ app/api/restaurants/[id]/menus/[menuId]/menuItems/[menuItemId]/route.ts
@@ -14,3 +14,20 @@ export async function GET(request: Request, { params: { id, menuId,menuItemId } 
 }
 
 
+export async function PUT(request: Request, { params: { id, menuId,menuItemId } }: { params: { id: string; menuId: string; menuItemId:string  } }) {
+
+  const json = await request.json();
+
+  const updated = await prisma.menuItem.update({
+    where: {
+      id: parseInt(menuItemId, 10)
+    },
+    data: {
+      name: json.name || null,
+      description: json.description || null,
+      price: json.price || null
+    }
+  })
+
+  return NextResponse.json(updated)
+}
